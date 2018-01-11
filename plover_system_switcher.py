d5190feed4a6d38c73b56686019d930a63159046
Nicholas Markopoulos <nsmarkop@gmail.com>
2018-01-10 20:55:30 -0600

    Add parameter documentation for the system switch function

--- plover_system_switcher.py
+++ plover_system_switcher.py
@@ -8,6 +8,12 @@ from plover.engine import StenoEngine
 def switch_system(engine: StenoEngine, system: str) -> None:
     '''
     Command to switch the current system in Plover.
+
+    :param engine: The active Plover engine that is executing the command.
+    :type engine: plover.engine.StenoEngine
+
+    :param system: The name of the requested system to switch Plover to using.
+    :type system: str
     '''
 
     engine.config = {'system_name': system}
